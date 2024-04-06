e1b2848f5f0cec71a2861a0882068130c01731d4
A. M Samdani Mozumder <114305338+samdani91@users.noreply.github.com>
2024-04-06 21:15:35 +0600

    Not Completed

---
+++ main.js
@@ -0,0 +1,7 @@
+var Typed= new Typed(".text",{
+    strings:["Software Engineer","Competitive Programmer","Full Stack Developer","App Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
